c5aa5ce7e2f5442955a05592e805f6bf80b3fc28
Clinged1 <nervousblob3@gmail.com>
2024-11-13 20:26:41 -0500

    Initial commit of Basketball project

--- basketball.js
+++ basketball.js
@@ -1,3 +1,5 @@
+
+
 function one() {
     let changeScore = document.getElementById("homeScore");
     changeScore.textContent = Number(changeScore.textContent) + 1;
@@ -8,15 +10,19 @@ function two() {
     changeScore.textContent = Number(changeScore.textContent) + 2;
 }
 
+
 function three() {
     let changeScore = document.getElementById("homeScore");
     changeScore.textContent = Number(changeScore.textContent) + 3;
+
 }
+
 
 function reset() {
     let changeScore = document.getElementById("homeScore");
     changeScore.textContent = 0;
 }
+
 
 function onealt() {
     let changeScore = document.getElementById("awayScore");
@@ -28,12 +34,21 @@ function twoalt() {
     changeScore.textContent = Number(changeScore.textContent) + 2;
 }
 
+
 function threealt() {
     let changeScore = document.getElementById("awayScore");
     changeScore.textContent = Number(changeScore.textContent) + 3;
+
 }
+
 
 function resetalt() {
     let changeScore = document.getElementById("awayScore");
     changeScore.textContent = 0;
 }
+
+
+
+
+
+
